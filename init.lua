3f9234b327d47c0d15910ae44503ea63c722958a
aoikurokawa <ht.so.ik0102@gmail.com>
2025-02-24 08:42:10 +0900

    fix: autoformat

--- init.lua
+++ init.lua
@@ -648,7 +648,7 @@ require('lazy').setup({
           -- if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
           --   local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
           --   vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-           --    buffer = event.buf,
+          --    buffer = event.buf,
           --     group = highlight_augroup,
           --     callback = vim.lsp.buf.document_highlight,
           --   })
@@ -733,8 +733,9 @@ require('lazy').setup({
           settings = {
             completion = {
               autoimport = true,
-            }
-          }
+            },
+            format = true,
+          },
         },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -797,78 +798,78 @@ require('lazy').setup({
     end,
   },
 
-  -- { -- Autoformat
-  --   'stevearc/conform.nvim',
-  --   event = { 'BufWritePre' },
-  --   cmd = { 'ConformInfo' },
-  --   keys = {
-  --     {
-  --       '<leader>f',
-  --       function()
-  --         require('conform').format { async = true, lsp_format = 'fallback' }
-  --       end,
-  --       mode = '',
-  --       desc = '[F]ormat buffer',
-  --     },
-  --   },
-  --   opts = {
-  --     notify_on_error = false,
-  --     format_on_save = function(bufnr)
-  --       -- Disable "format_on_save lsp_fallback" for languages that don't
-  --       -- have a well standardized coding style. You can add additional
-  --       -- languages here or re-enable it for the disabled ones.
-  --       local disable_filetypes = { c = true, cpp = true }
-  --       local lsp_format_opt
-  --       if disable_filetypes[vim.bo[bufnr].filetype] then
-  --         lsp_format_opt = 'never'
-  --       else
-  --         lsp_format_opt = 'fallback'
-  --       end
-  --       return {
-  --         timeout_ms = 500,
-  --         lsp_format = lsp_format_opt,
-  --       }
-  --     end,
-  --     formatters_by_ft = {
-  --       lua = { 'stylua' },
-  --       -- Conform can also run multiple formatters sequentially
-  --       -- python = { "isort", "black" },
-  --       --
-  --       -- You can use 'stop_after_first' to run the first available formatter from the list
-  --       -- javascript = { "prettierd", "prettier", stop_after_first = true },
-  --     },
-  --   },
-  -- },
+  { -- Autoformat
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        '<leader>f',
+        function()
+          require('conform').format { async = true, lsp_format = 'fallback' }
+        end,
+        mode = '',
+        desc = '[F]ormat buffer',
+      },
+    },
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+        local lsp_format_opt
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          lsp_format_opt = 'never'
+        else
+          lsp_format_opt = 'fallback'
+        end
+        return {
+          timeout_ms = 500,
+          lsp_format = lsp_format_opt,
+        }
+      end,
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        -- Conform can also run multiple formatters sequentially
+        -- python = { "isort", "black" },
+        --
+        -- You can use 'stop_after_first' to run the first available formatter from the list
+        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+    },
+  },
 
-   { -- Autocompletion
-     'hrsh7th/nvim-cmp',
-     event = 'InsertEnter',
-     dependencies = {
-       -- Snippet Engine & its associated nvim-cmp source
-       {
-         'L3MON4D3/LuaSnip',
-         build = (function()
-           -- Build Step is needed for regex support in snippets.
-           -- This step is not supported in many windows environments.
-           -- Remove the below condition to re-enable on windows.
-           if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-             return
-           end
-           return 'make install_jsregexp'
-         end)(),
-         dependencies = {
-           -- `friendly-snippets` contains a variety of premade snippets.
-           --    See the README about individual language/framework/plugin snippets:
-           --    https://github.com/rafamadriz/friendly-snippets
-           -- {
-           --   'rafamadriz/friendly-snippets',
-           --   config = function()
-           --     require('luasnip.loaders.from_vscode').lazy_load()
-           --   end,
-           -- },
-         },
-       },
-       'saadparwaiz1/cmp_luasnip',
+  { -- Autocompletion
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+      -- Snippet Engine & its associated nvim-cmp source
+      {
+        'L3MON4D3/LuaSnip',
+        build = (function()
+          -- Build Step is needed for regex support in snippets.
+          -- This step is not supported in many windows environments.
+          -- Remove the below condition to re-enable on windows.
+          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+            return
+          end
+          return 'make install_jsregexp'
+        end)(),
+        dependencies = {
+          -- `friendly-snippets` contains a variety of premade snippets.
+          --    See the README about individual language/framework/plugin snippets:
+          --    https://github.com/rafamadriz/friendly-snippets
+          -- {
+          --   'rafamadriz/friendly-snippets',
+          --   config = function()
+          --     require('luasnip.loaders.from_vscode').lazy_load()
+          --   end,
+          -- },
+        },
+      },
+      'saadparwaiz1/cmp_luasnip',
 
       -- Adds other completion capabilities.
       --  nvim-cmp does not ship with all sources by default. They are split
@@ -883,62 +884,62 @@ require('lazy').setup({
       local luasnip = require 'luasnip'
       luasnip.config.setup {}
 
-       cmp.setup {
-         snippet = {
-           expand = function(args)
-             luasnip.lsp_expand(args.body)
-           end,
-         },
-         completion = { completeopt = 'menu,menuone,noinsert' },
+      cmp.setup {
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        completion = { completeopt = 'menu,menuone,noinsert' },
 
-         -- For an understanding of why these mappings were
-         -- chosen, you will need to read `:help ins-completion`
-         --
-         -- No, but seriously. Please read `:help ins-completion`, it is really good!
-         mapping = cmp.mapping.preset.insert {
-           -- Select the [n]ext item
-           ['<C-n>'] = cmp.mapping.select_next_item(),
-           -- Select the [p]revious item
-           ['<C-p>'] = cmp.mapping.select_prev_item(),
+        -- For an understanding of why these mappings were
+        -- chosen, you will need to read `:help ins-completion`
+        --
+        -- No, but seriously. Please read `:help ins-completion`, it is really good!
+        mapping = cmp.mapping.preset.insert {
+          -- Select the [n]ext item
+          ['<C-n>'] = cmp.mapping.select_next_item(),
+          -- Select the [p]revious item
+          ['<C-p>'] = cmp.mapping.select_prev_item(),
 
-           -- Scroll the documentation window [b]ack / [f]orward
-           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-           ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          -- Scroll the documentation window [b]ack / [f]orward
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
 
-           -- Accept ([y]es) the completion.
-           --  This will auto-import if your LSP supports it.
-           --  This will expand snippets if the LSP sent a snippet.
-           ['<C-y>'] = cmp.mapping.confirm { select = true },
+          -- Accept ([y]es) the completion.
+          --  This will auto-import if your LSP supports it.
+          --  This will expand snippets if the LSP sent a snippet.
+          ['<C-y>'] = cmp.mapping.confirm { select = true },
 
-           -- If you prefer more traditional completion keymaps,
-           -- you can uncomment the following lines
-           ['<CR>'] = cmp.mapping.confirm { select = true },
-           --['<Tab>'] = cmp.mapping.select_next_item(),
-           --['<S-Tab>'] = cmp.mapping.select_prev_item(),
+          -- If you prefer more traditional completion keymaps,
+          -- you can uncomment the following lines
+          ['<CR>'] = cmp.mapping.confirm { select = true },
+          --['<Tab>'] = cmp.mapping.select_next_item(),
+          --['<S-Tab>'] = cmp.mapping.select_prev_item(),
 
-           -- Manually trigger a completion from nvim-cmp.
-           --  Generally you don't need this, because nvim-cmp will display
-           --  completions whenever it has completion options available.
-           ['<C-Space>'] = cmp.mapping.complete {},
+          -- Manually trigger a completion from nvim-cmp.
+          --  Generally you don't need this, because nvim-cmp will display
+          --  completions whenever it has completion options available.
+          ['<C-Space>'] = cmp.mapping.complete {},
 
-           -- Think of <c-l> as moving to the right of your snippet expansion.
-           --  So if you have a snippet that's like:
-           --  function $name($args)
-           --    $body
-           --  end
-           --
-           -- <c-l> will move you to the right of each of the expansion locations.
-           -- <c-h> is similar, except moving you backwards.
-           ['<C-l>'] = cmp.mapping(function()
-             if luasnip.expand_or_locally_jumpable() then
-               luasnip.expand_or_jump()
-             end
-           end, { 'i', 's' }),
-           ['<C-h>'] = cmp.mapping(function()
-             if luasnip.locally_jumpable(-1) then
-               luasnip.jump(-1)
-             end
-           end, { 'i', 's' }),
+          -- Think of <c-l> as moving to the right of your snippet expansion.
+          --  So if you have a snippet that's like:
+          --  function $name($args)
+          --    $body
+          --  end
+          --
+          -- <c-l> will move you to the right of each of the expansion locations.
+          -- <c-h> is similar, except moving you backwards.
+          ['<C-l>'] = cmp.mapping(function()
+            if luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            end
+          end, { 'i', 's' }),
+          ['<C-h>'] = cmp.mapping(function()
+            if luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            end
+          end, { 'i', 's' }),
 
           -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
           --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
@@ -961,51 +962,51 @@ require('lazy').setup({
   {
     'loctvl842/monokai-pro.nvim',
     config = function()
-    require("monokai-pro").setup({
-      transparent_background = false,
-      terminal_colors = true,
-      devicons = true, -- highlight the icons of `nvim-web-devicons`
-      styles = {
-        comment = { italic = true },
-        keyword = { italic = true }, -- any other keyword
-        type = { italic = true }, -- (preferred) int, long, char, etc
-        storageclass = { italic = true }, -- static, register, volatile, etc
-        structure = { italic = true }, -- struct, union, enum, etc
-        parameter = { italic = true }, -- parameter pass in function
-        annotation = { italic = true },
-        tag_attribute = { italic = true }, -- attribute of tag in reactjs
-      },
-      filter = "classic", -- classic | octagon | pro | machine | ristretto | spectrum
-      -- Enable this will disable filter option
-      day_night = {
-        enable = false, -- turn off by default
-        day_filter = "classic", -- classic | octagon | pro | machine | ristretto | spectrum
-        night_filter = "classic", -- classic | octagon | pro | machine | ristretto | spectrum
-      },
-      inc_search = "background", -- underline | background
-      background_clear = {
-        -- "float_win",
-        "toggleterm",
-        "telescope",
-        -- "which-key",
-        "renamer",
-        "notify",
-        -- "nvim-tree",
-        "neo-tree",
-        "bufferline", -- better used if background of `neo-tree` or `nvim-tree` is cleared
-      },-- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree", "nvim-tree", "bufferline"
-      plugins = {
-        bufferline = {
-          underline_selected = false,
-          underline_visible = false,
+      require('monokai-pro').setup {
+        transparent_background = false,
+        terminal_colors = true,
+        devicons = true, -- highlight the icons of `nvim-web-devicons`
+        styles = {
+          comment = { italic = true },
+          keyword = { italic = true }, -- any other keyword
+          type = { italic = true }, -- (preferred) int, long, char, etc
+          storageclass = { italic = true }, -- static, register, volatile, etc
+          structure = { italic = true }, -- struct, union, enum, etc
+          parameter = { italic = true }, -- parameter pass in function
+          annotation = { italic = true },
+          tag_attribute = { italic = true }, -- attribute of tag in reactjs
         },
-        indent_blankline = {
-          context_highlight = "default", -- default | pro
-          context_start_underline = false,
+        filter = 'classic', -- classic | octagon | pro | machine | ristretto | spectrum
+        -- Enable this will disable filter option
+        day_night = {
+          enable = false, -- turn off by default
+          day_filter = 'classic', -- classic | octagon | pro | machine | ristretto | spectrum
+          night_filter = 'classic', -- classic | octagon | pro | machine | ristretto | spectrum
         },
-      },
-    })
-      vim.cmd("colorscheme monokai-pro")
+        inc_search = 'background', -- underline | background
+        background_clear = {
+          -- "float_win",
+          'toggleterm',
+          'telescope',
+          -- "which-key",
+          'renamer',
+          'notify',
+          -- "nvim-tree",
+          'neo-tree',
+          'bufferline', -- better used if background of `neo-tree` or `nvim-tree` is cleared
+        }, -- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree", "nvim-tree", "bufferline"
+        plugins = {
+          bufferline = {
+            underline_selected = false,
+            underline_visible = false,
+          },
+          indent_blankline = {
+            context_highlight = 'default', -- default | pro
+            context_start_underline = false,
+          },
+        },
+      }
+      vim.cmd 'colorscheme monokai-pro'
     end,
   },
 
