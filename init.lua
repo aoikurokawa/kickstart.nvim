3b66b32a6bc7abb9d548fa790da0ae092f335b9c
Aoi Kurokawa <62386689+aoikurokawa@users.noreply.github.com>
2025-03-01 09:25:13 +0900

    Fix: enable tree (#9)

--- init.lua
+++ init.lua
@@ -452,69 +452,69 @@ require('lazy').setup({
     end,
   },
 
-  -- {
-  --   'nvim-neo-tree/neo-tree.nvim',
-  --   branch = 'v3.x',
-  --   dependencies = {
-  --     'nvim-lua/plenary.nvim',
-  --     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-  --     'MunifTanjim/nui.nvim',
-  --     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  --   },
-  --   config = function()
-  --     require('neo-tree').setup {
-  --       default_component_configs = {
-  --         container = {
-  --           enable_character_fade = true,
-  --         },
-  --         indent = {
-  --           indent_size = 2,
-  --           padding = 1, -- extra padding on left hand side
-  --           -- indent guides
-  --           with_markers = true,
-  --           indent_marker = '│',
-  --           last_indent_marker = '└',
-  --           highlight = 'NeoTreeIndentMarker',
-  --           -- expander config, needed for nesting files
-  --           with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
-  --           expander_collapsed = '',
-  --           expander_expanded = '',
-  --           expander_highlight = 'NeoTreeExpander',
-  --         },
-  --         icon = {
-  --           folder_closed = '►',
-  --           folder_open = '▼',
-  --           folder_empty = '-',
-  --           -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
-  --           -- then these will never be used.
-  --           default = '*',
-  --           highlight = 'NeoTreeFileIcon',
-  --         },
-  --         modified = {
-  --           symbol = '[+]',
-  --           highlight = 'NeoTreeModified',
-  --         },
-  --         git_status = {
-  --           symbols = {
-  --             -- Change type
-  --             added = '', -- or "✚", but this is redundant info if you use git_status_colors on the name
-  --             modified = '', -- or "", but this is redundant info if you use git_status_colors on the name
-  --             deleted = '✖', -- this can only be used in the git_status source
-  --             renamed = '󰁕', -- this can only be used in the git_status source
-  --             -- Status type
-  --             untracked = '',
-  --             ignored = '',
-  --             unstaged = '󰄱',
-  --             staged = '',
-  --             conflict = '',
-  --           },
-  --         },
-  --       },
-  --     }
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function()
+      require('neo-tree').setup {
+        default_component_configs = {
+          container = {
+            enable_character_fade = true,
+          },
+          indent = {
+            indent_size = 2,
+            padding = 1, -- extra padding on left hand side
+            -- indent guides
+            with_markers = true,
+            indent_marker = '│',
+            last_indent_marker = '└',
+            highlight = 'NeoTreeIndentMarker',
+            -- expander config, needed for nesting files
+            with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
+            expander_collapsed = '',
+            expander_expanded = '',
+            expander_highlight = 'NeoTreeExpander',
+          },
+          icon = {
+            folder_closed = '►',
+            folder_open = '▼',
+            folder_empty = '-',
+            -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
+            -- then these will never be used.
+            default = '*',
+            highlight = 'NeoTreeFileIcon',
+          },
+          modified = {
+            symbol = '[+]',
+            highlight = 'NeoTreeModified',
+          },
+          git_status = {
+            symbols = {
+              -- Change type
+              added = '', -- or "✚", but this is redundant info if you use git_status_colors on the name
+              modified = '', -- or "", but this is redundant info if you use git_status_colors on the name
+              deleted = '✖', -- this can only be used in the git_status source
+              renamed = '󰁕', -- this can only be used in the git_status source
+              -- Status type
+              untracked = '',
+              ignored = '',
+              unstaged = '󰄱',
+              staged = '',
+              conflict = '',
+            },
+          },
+        },
+      }
 
-  --     -- vim.cmd [[nnoremap \ :Neotree reveal<cr>]]
-  --   end,
-  -- },
+      -- vim.cmd [[nnoremap \ :Neotree reveal<cr>]]
+    end,
+  },
 
   -- LSP Plugins
   {
